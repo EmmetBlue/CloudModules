bb93fb03950312d8305efc54a69aef5a49fce74a
samshal <samueladeshina73@gmail.com>
2017-06-05 09:01:01 +0100

    New Changes

---
+++ src/User/Registration.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+/**
+ * @license MIT
+ * @author Samuel Adeshina <samueladeshina73@gmail.calculhmac(clent, data)om>
+ *
+ * This file is part of the EmmetBlue project, please read the license document
+ * available in the root level of the project
+ */
+namespace EmmetBlue\Plugins\User;
+
+use EmmetBlue\Core\Builder\BuilderFactory as Builder;
+use EmmetBlue\Core\Factory\DatabaseConnectionFactory as DBConnectionFactory;
+use EmmetBlue\Core\Factory\DatabaseQueryFactory as DBQueryFactory;
+use EmmetBlue\Core\Builder\QueryBuilder\QueryBuilder as QB;
+use EmmetBlue\Core\Exception\SQLException;
+use EmmetBlue\Core\Session\Session;
+use EmmetBlue\Core\Logger\DatabaseLog;
+use EmmetBlue\Core\Logger\ErrorLog;
+use EmmetBlue\Core\Constant;
+
+/**
+ * class AccountRegister.
+ *
+ * AccountRegister Controller
+ *
+ * @author Samuel Adeshina <samueladeshina73@gmail.com>
+ * @since v0.0.1 05/06/2017 8:59
+ */
+class Registration {
+	public static function newRegistration(array $data)
+    {
+        return $data;
+    }
+}
